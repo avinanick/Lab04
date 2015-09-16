9dbc3da79b582edade9d01ff2f8f7cafa1ab3afb
Valenzuela <eric.valenzuela798@sie-1222-15.csuci.edu>
2015-09-16 13:23:07 -0700

    adding new files

---
+++ HourlyEmployee.java
@@ -0,0 +1,35 @@
+package salarydemo;
+/**
+   An hourly employee is paid for every hour worked.
+*/
+public class HourlyEmployee extends Employee
+{
+   private double hourlyWage;
+
+   /**
+      Constructs an hourly employee with a given name and weekly wage.
+      @param name the name of this employee
+      @param wage the hourly wage 
+   */
+   public HourlyEmployee(String name, double wage)
+   {
+      setName(name);
+      hourlyWage = wage;
+   }
+ 
+   /**
+	 * 
+	 * @param extraHours
+	 */
+	public double overtimeCalc(int extraHours)
+   {
+      double pay = hoursWorked * hourlyWage;
+      if (hoursWorked > 40)
+      {
+         // Add overtime
+         pay = pay + ((hoursWorked - 40) * 0.5) * hourlyWage;
+      }
+      return pay;
+   }
+}
+
---
+++ StudentEmployee.java
@@ -0,0 +1,26 @@
+package salarydemo;
+
+public class StudentEmployee extends HourlyEmployee {
+
+	private double semesterSalary;
+
+	/**
+	 * 
+	 * @param name
+	 * @param pay
+	 */
+	public StudentEmployee(String name, double pay) {
+		// TODO - implement StudentEmployee.StudentEmployee
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param extraHours
+	 */
+	public double overtimeCalc(int extraHours) {
+		// TODO - implement StudentEmployee.overtimeCalc
+		throw new UnsupportedOperationException();
+	}
+
+}
